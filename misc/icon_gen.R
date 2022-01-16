3a5cd2b38d833d9909db276af7f152753ecec9c4
Ashley Naimi <ashley.naimi@emory.edu>
2022-01-16 14:43:44 -0500

    Remove the misc folder from git tracking

--- misc/icon_gen.R
+++
@@ -1,8 +0,0 @@
-remotes::install_github("rstudio/fontawesome")
-library(fontawesome)
-
-## not working...
-
-png("./figures/fa-lightbulb.png")
-fa(name="lightbulb", fill="black")
-dev.off()
--- misc/qr_gen.R
+++
@@ -1,6 +0,0 @@
-install.packages("qrcode")
-library(qrcode)
-
-png("qrplot.png")
-qrcode_gen("https://datascienceplus.com/")
-dev.off()
