c7e70e9004ae7ec0f81660b62245f6ba8facfa84
Ashley Naimi <ashley.naimi@emory.edu>
2023-08-25 12:04:02 -0400

    re-formated the deploy shell script to EPI560

--- _deploy.sh
+++ _deploy.sh
@@ -3,10 +3,10 @@
 # clone the repository
 # if [ $1 == 'local' ]
 # then
-mkdir IMCS
+mkdir EPI560
 git clone -b gh-pages \
-  https://github.com/ainaimi/IMCS \
-  IMCS
+  https://github.com/ainaimi/EPI560 \
+  EPI560
 # else
 # 	# configure your name and email if you have not done so
 # 	git config --global user.email "ashley.naimi@emory.edu"
@@ -15,13 +15,13 @@ git clone -b gh-pages \
 
 # 	git clone -b gh-pages \
 #   https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git \
-#   IMCS
+#   EPI560
 # fi
 
 # remove contents from existing gh-pages branch
-cd IMCS
+cd EPI560
 git rm -rf *
-echo "All files in /IMCS after git rm"
+echo "All files in /EPI560 after git rm"
 ls -l 
 # replace with contents from master branch /website
 cp -r ../website/* ./
@@ -29,7 +29,7 @@ cp -r ../website/* ./
 cp -r ../lectures ./
 cp -r ../homework ./
 
-echo "All files in /IMCS after copies"
+echo "All files in /EPI560 after copies"
 ls -l 
 ls -l lectures
 ls -l homework
